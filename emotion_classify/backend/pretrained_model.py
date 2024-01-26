abf4274d5634bf444c96178455b6da4e55326fd6
Jay Bathiya <jkb9545@gmail.com>
2024-01-26 23:24:26 +0530

    version_trial

--- emotion_classify/backend/pretrained_model.py
+++ emotion_classify/backend/pretrained_model.py
@@ -8,8 +8,6 @@ input_text = "null"
 
 inputs = tokenizer(input_text, return_tensors="pt")
 
-# Forward pass through the model
 outputs = model(**inputs)
 
-# Apply softmax to get probabilities
 probs = torch.nn.functional.softmax(outputs.logits, dim=1)
