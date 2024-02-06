a23ae9384cf706ee3047e51403138c26c7cf58b6
Jay Bathiya <jkb9545@gmail.com>
2024-02-07 03:58:48 +0530

    v_1

---
+++ CNN_classification/ml_model/trial.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np 
+import torch
+import glob
+import torch.nn as nn 
+from torchvision.transforms import transforms
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.autograd import Variable 
+import torchvision
+import pathlib
